97e02ba0341604f1b4b301081591501a890b47eb
gurustron <gurustronpublic@gmail.com>
2025-12-07 11:09:21 +0300

    Benchmarks

---
+++ NET10.Benchs/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using NET10.Benchs.StackAllocs;
+
+var config = DefaultConfig.Instance
+    .AddJob(Job.Default.WithId(".NET 9").WithRuntime(CoreRuntime.Core90).AsBaseline())
+    .AddJob(Job.Default.WithId(".NET 10").WithRuntime(CoreRuntime.Core10_0))
+    ;
+BenchmarkRunner.Run<ArrayStackAllocBench>(config);
