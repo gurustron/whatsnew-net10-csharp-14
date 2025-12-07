227fb7237481c6ebe9d8c2dcebc13e0f06229816
gurustron <gurustronpublic@gmail.com>
2025-12-07 10:34:33 +0300

    Initial solutions

---
+++ WhatsNewNET10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using WhatsNewNET10.Features;
+
+// NumericOrdering.Do();
+
+
+string[] things = ["paul", "bob", "lauren", "007", "90"];
+
+StringComparer numericStringComparer = 
+    StringComparer.Create(CultureInfo.CurrentCulture, CompareOptions.NumericOrdering);
+
+foreach (var item in things.Order(numericStringComparer))
+{
+    Console.WriteLine(item);
+}
