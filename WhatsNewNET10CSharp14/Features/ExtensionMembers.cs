660016265e152cff2587694dba6b53454209fd72
sstronchinskiy <sstronchinskiy@ozon.ru>
2025-12-13 15:02:47 +0300

    ---

--- WhatsNewNET10CSharp14/Features/ExtensionMembers.cs
+++ WhatsNewNET10CSharp14/Features/ExtensionMembers.cs
@@ -6,7 +6,7 @@ namespace WhatsNewCSharp14.Features;
 public static class ExtensionMembers
 {
     // only static, e.g. invoked on type - int.DoSomething()
-    extension(int /*receiver parameter*/) 
+    extension(int /*receiver parameter*/)
     {
         public static int DoSomething() => 42;
 
@@ -14,20 +14,21 @@ public static class ExtensionMembers
         // public int DoSomething() => 42;
     }
 
-    extension(long) 
+    extension(long)
     {
         // Type 'ExtensionMembers' already defines a member called
         // 'DoSomething' with the same parameter types
         // public static long DoSomething() => 42; // will not compile
     }
+
     // static and instance
-    extension(long name /*named receiver parameter*/) 
+    extension(long name /*named receiver parameter*/)
     {
         public long DoSomething() => name; // 42L.DoSomething()
         public static long DoSomethingLong() => 42;
     }
 
-    extension(int name /*named receiver parameter*/) 
+    extension(int name /*named receiver parameter*/)
     {
         // Type 'MyExtensionMembers' already defines a member called 'DoSomething'
         // with the same parameter types
@@ -60,7 +61,7 @@ public static class E
         // with the same name as a type parameter or receiver parameter of the extension declaration.
         // public void M3(int T, string ts) { }          // Error: Cannot reuse names `T` and `ts`
         // public void M4<T, ts>(string s) { }           // Error: Cannot reuse names `T` and `ts`
-        
+
         // Though can be used as names
         public void T() { M(ts); } // Generated static method M<T>(T[]) is found
         public void ts() { }
@@ -69,7 +70,7 @@ public static class E
         {
             // ts(); // Error: Method, delegate, or event is expected
             // T(ts); // Error: T is a type parameter
-        } 
+        }
     }
 
     extension<T>(T) where T : INumber<T>
@@ -81,6 +82,6 @@ public static class E
             {
                 yield return start++;
             }
-        } 
+        }
     }
 }
