a4c21f357242d08fb85e3a016f40dade8a0bd857
sstronchinskiy <sstronchinskiy@ozon.ru>
2025-12-13 22:32:55 +0300

    NumericOrderings Demo

---
+++ WhatsNewNET10.DemoRunner/Demos/NumericOrderingsDemos.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace WhatsNewNET10.DemoRunner.Demos;
+
+public class NumericOrderingsDemos
+{
+    public static void Do()
+    {
+        string[] things = ["paul", "bob", "lauren", "107", "90"];
+
+        var numericStringComparer =
+            StringComparer.Create(CultureInfo.InvariantCulture, CompareOptions.NumericOrdering);
+
+        foreach (var item in things.Order(numericStringComparer))
+        {
+            Console.WriteLine(item);
+        }
+
+        Environment.Exit(0);
+    }
+}
