9f59a894dc2b3a042b3cb284385421b0994aaa48
gurustron <gurustronpublic@gmail.com>
2025-12-13 10:07:51 +0300

    Adjust DemoRunner for updated benchmarking setup: increase cycles, add version info, suppress CS8321 warnings, and improve inline methods for stack allocation

--- WhatsNewNET10.DemoRunner/Program.cs
+++ WhatsNewNET10.DemoRunner/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
+// dotnet run -c Release -f net9.0
+// dotnet run -c Release -f net10.0
+// export DOTNET_JitDisasmSummary=1
 const int Iterations = 100_000;
-const int Cycles = 15;
+const int Cycles = 30;
 
-Console.WriteLine($"Running, {Iterations} per cycle, {Cycles} cycles...");
+Console.WriteLine($"Running {Environment.Version}, {Iterations} per cycle, {Cycles} cycles...");
 
 var sw = Stopwatch.StartNew();
 
@@ -21,12 +24,44 @@ for (int c = 0; c < Cycles; c++)
     
     var diffMem = GC.GetAllocatedBytesForCurrentThread() - allocated;
     var elapsed = sw.Elapsed.TotalNanoseconds;
-    Console.WriteLine($"{elapsed / Iterations:N0} ns, {diffMem / Iterations:N0} bytes");
+    Console.WriteLine($"{c:00}: {elapsed / Iterations:N0} ns, {diffMem / Iterations:N0} bytes");
+} 
+[MethodImpl(MethodImplOptions.NoInlining)]
+static void Test() // g__Test|0_0
+{
+    Process(new string[] { "a", "b", "c" });
+
+    // Should be inlined which will allow the array to be stack allocated
+    static void Process(string[] inputs) 
+    {
+        foreach (string input in inputs)
+        {
+            Use(input);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void Use(string input)
+        {
+        }
+    }
 }
 
-
 [MethodImpl(MethodImplOptions.NoInlining)]
-static void Test()
+static void TestStackObjectAllocations()
 {
-    
+    Process(new string[] { "a", "b", "c" });
+
+    // Should be inlined which will allow the array to be stack allocated
+    static void Process(string[] inputs) 
+    {
+        foreach (string input in inputs)
+        {
+            Use(input);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        static void Use(string input)
+        {
+        }
+    }
 }
